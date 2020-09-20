c0a81f679edbb3af91ea486ee4d5e9b1dde1cd12
rfrech <rfrechette@cnm.edu>
2020-09-20 00:10:03 -0700

    Added solute method and test for command line arguments

--- src/edu/cnm/deepdive/prework/HelloWorld.java
+++ src/edu/cnm/deepdive/prework/HelloWorld.java
@@ -3,11 +3,11 @@ package edu.cnm.deepdive.prework;
 public class HelloWorld {
 
   public static void main(String[] args) {
-    salute("World");
+    salute((args.length > 0) ? args[0] : "World");
   }
 
   private static void salute(String target) {
-    System.out.println("Hello " + target + "!");
+    System.out.println("Hello, " + target + "!");
   }
 
 }
